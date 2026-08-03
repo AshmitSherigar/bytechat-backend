4bb92b8d3a5ab71829c2aa269c6c73ab18540852
AshmitSherigar <sherigarashmit@gmail.com>
2026-08-03 16:42:57 +0530

    fix: minimum password requirement

--- src/validators/auth.validator.ts
+++ src/validators/auth.validator.ts
@@ -1,6 +1,6 @@
 import * as z from 'zod';
 
 export const userSchema = z.object({
-  username: z.string().max(12),
-  password: z.string().max(10),
+  username: z.string().max(12).min(1),
+  password: z.string().max(10).min(5),
 });
