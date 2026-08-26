115804c53b46949d95d632634ade8c0d0f8837bf
AshmitSherigar <sherigarashmit@gmail.com>
2026-08-27 01:16:28 +0530

    feat: implemented auth : login and register

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './db/db.js';
+import { router as authRoutes } from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -9,6 +10,10 @@ await connectDB();
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.use('/api/auth', authRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running http://localhost:${port}`);
