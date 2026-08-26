7f52cdc7e3750f2db48607a2a86ff674e6aa7573
AshmitSherigar <sherigarashmit@gmail.com>
2026-08-26 22:33:40 +0530

    feat: implemented connection to database

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import connectDB from './db/db.js';
+
+dotenv.config();
+
+await connectDB();
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
   console.log(`Server is running http://localhost:${port}`);
